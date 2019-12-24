4803d77499a29d57f5ae6616a87dc647e0675cf3
rgordeev <roma.gordeev@gmail.com>
2019-12-24 20:14:08 +0300

    Images saples

--- app/src/main/java/ru/rgordeev/converter/MainActivity.java
+++ app/src/main/java/ru/rgordeev/converter/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,56 +20,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void validatePassword(View view) {
-
-
-        Log.i("Info", "password validation");
-
-        TextView password = findViewById(R.id.userPassword);
-        String pass = password.getText().toString();
-
-        if (isPasswordStrong(pass)) {
-            Log.i("Info", "Our password is strong");
-        }
-
-    }
-
-    public void validateUserName(View view) {
-
-        Log.i("Info", "Component TextView was pressed");
-    }
-
-    private boolean isPasswordStrong(String password) {
-        String[] numbers = "0;1;2;3;4;5;6;7;8;9".split(";");
-        String[] letters = "a;b;c;d;e;f;g;h;i;j;k;l;m;n;o;p;q;r;s;t;u;v;w;x;y;z".split(";");
-        boolean containsNumbers = false;
-        boolean containsLetters = false;
-
-        for (String number: numbers) {
-            if (password.contains(number)) {
-                containsNumbers = true;
-                break;
-            }
-        }
-
-        for (String letter: letters) {
-            if (password.contains(letter)) {
-                containsLetters = true;
-                break;
-            }
-        }
-
-        return containsLetters && containsNumbers;
-    }
-
-    public void showData(View view) {
-        TextView userName = findViewById(R.id.userName);
-        TextView userPassword = findViewById(R.id.userPassword);
-
-        Toast toast = Toast.makeText(this, String.format("UserName: %s UserPassword: %s",
-                userName.getText().toString(), userPassword.getText().toString()),
-                Toast.LENGTH_SHORT);
-        toast.setMargin(30, 30);
-        toast.show();
+    public void onButtonClick(View view) {
+        ImageView catImage = findViewById(R.id.catImage);
+        catImage.setImageResource(R.drawable.cat2);
     }
 }
